77f2b763158d03edcc72236388b1265b23cd1ada
양윤직 <yang-yunjig@yang-yunjig-ui-MacBookPro.local>
2025-07-02 21:52:32 +0900

    minit good commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -20,9 +20,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/", // ✅ 반드시 있어야 Netlify 경로 문제 해결
   plugins: [react()],
-  base: "/", // ✅ 반드시 명시 (경로 문제 해결)
-  server: {
-    port: 5173,
-  },
 });
